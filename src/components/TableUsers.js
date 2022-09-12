20fd686025e644ca11c7e751562de331e0dfafe1
QuangNey <quangney12602@gmail.com>
2022-09-13 03:52:45 +0700

    customize-axios

--- src/components/TableUsers.js
+++ src/components/TableUsers.js
@@ -14,13 +14,11 @@ const TableUsers = (props) => {
 
     const getUsers = async () => {
         let res = await fetchAllUser();
-
-        if(res && res.data && res.data.data) {
-            setListUsers(res.data.data);
+        if(res && res.data) {
+            setListUsers(res.data);
         }
     }
 
-    console.log(listUsers);
     return (<>
         <Table striped bordered hover>
             <thead>
--- src/services/axios.js
+++
@@ -1,6 +0,0 @@
-
-const fetchAllUser = () => {
-
-}
-
-export { fetchAllUser };
